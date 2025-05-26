d88330f24d96b95a0fd20cd37be02fd9d588a723
claracorrea <clara.correa.torres1@gmail.com>
2025-05-26 17:07:05 -0300

    script para crear bd

---
+++ persistencia/bd.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE zappyMenuDeJuegos;
+USE zappyMenuDeJuegos;
+
+CREATE TABLE administrador (
+  permisos varchar(13) DEFAULT NULL
+);
+
+CREATE TABLE juego (
+  id int(11) NOT NULL PRIMARY KEY,
+  nombre varchar(30) NOT NULL,
+  recompensa varchar(50) NOT NULL,
+  tipo enum('memoria','trivia','azaroso','puertaslocas','piedra_papel_o_tijera') NOT NULL
+);
+
+CREATE TABLE usuario (
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre_usuario varchar(20) NOT NULL,
+  contraseña varchar(20) NOT NULL,
+  permisos enum('Administrador','Jugador') NOT NULL
+);
