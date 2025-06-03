3ab0c784967bfdfa6413b4b71bf0f5d25cf337f2
claracorrea <clara.correa.torres1@gmail.com>
2025-06-03 18:03:56 -0300

    añadir modal de selección de trivia y ajustar lógica de apertura en logica.js

--- index/js/logica.js
+++ index/js/logica.js
@@ -1,12 +1,8 @@
 var modal = document.getElementById("modal01");
 function abrir() {
-    modal.style.display = "block";
+    //modal.style.display = "block";
+    modal.style.display = "flex";
 }
-function cerrar() {
+function volver() {
     modal.style.display = "none";
-}
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 }
