76d68e0bafaf201ac0497c885f10f2c598aa4826
claraaaa2008 <clara.correa.torres1@gmail.com>
2025-07-11 16:08:17 -0300

    sacar comentario innecesario

--- sitios/index/js/logica.js
+++ sitios/index/js/logica.js
@@ -1,8 +1,9 @@
 var modal = document.getElementById("modal01");
 function abrir() {
-    //modal.style.display = "block";
+    //el modal se depliega al hacer click en el botón
     modal.style.display = "flex";
 }
 function volver() {
+    //el modal se cierra al hacer click en el botón
     modal.style.display = "none";
 }
