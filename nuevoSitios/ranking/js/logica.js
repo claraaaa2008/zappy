33c366393012356936da5714547c0c7bfa90add4
claraaaa2008 <clara.correa.torres1@gmail.com>
2025-10-17 11:22:58 -0300

    comentar codigo ranking

--- nuevoSitios/ranking/js/logica.js
+++ nuevoSitios/ranking/js/logica.js
@@ -1,12 +1,13 @@
+/*** Efecto parallax para scroll ***/
 document.addEventListener('DOMContentLoaded', function () {
-    const rankingAll = document.querySelector('.rankingAll');
-    rankingAll.classList.remove('visible');
+    const rankingAll = document.querySelector('.rankingAll'); // Llama a la clase .rankingAll en html
+    rankingAll.classList.remove('visible'); // Quita la visibilidad a la clase .rankingAll al cargar la página
 
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 50) {
-            rankingAll.classList.add('visible');
-        } else {
-            rankingAll.classList.remove('visible');
+    window.addEventListener('scroll', function () { // Agrega un evento al scrollear la página
+        if (window.scrollY > 50) { // Si se scrollea mas de 50px
+            rankingAll.classList.add('visible'); // Se hace visible la clase .rankingAll
+        } else { // Caso contrario
+            rankingAll.classList.remove('visible'); // Caso contrario se quita la visibilidad a la clase .rankingAll
         }
     });
 });
